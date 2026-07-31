cc5df00be0a75c9bd213ebb43c64002f50c3ae2a
Jack Woehr <jack@seidengroup.com>
2026-07-31 17:55:52 -0400

    bash script that does Gemini as openai_compatible

---
+++ src/LLMRPG/gemini_openai_compat.sh
@@ -0,0 +1,13 @@
+curl "https://generativelanguage.googleapis.com:443/v1beta/openai/chat/completions" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $GEMINI_API_KEY" \
+  -d '{
+    "model": "gemini-3.6-flash",
+    "messages": [
+      {
+        "role": "user",
+        "content": "Explain to me how AI works"
+      }
+    ]
+  }'
+
